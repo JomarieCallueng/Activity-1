80a7bf983c6d068dc4f6187509aba448506deb7b
Jomariecallueng <jomarie.callueng.college@ustangelicum.edu.ph>
2024-12-14 14:38:05 +0800

    first commit

---
+++ Arithmetic/main.py
@@ -0,0 +1,15 @@
+J = int (input("Enter a number: "))
+
+N = int (input("Enter a number: "))
+
+sum = J + N 
+print(sum)
+        
+dif = J - N        
+print(dif)
+
+product = J * N 
+print(product)
+
+quotient = J / N
+print(quotient)
---
+++ BMI/main.py
@@ -0,0 +1,8 @@
+
+weight = float(input("Enter your weight: "))
+
+
+height = float(input("Enter your height: "))
+
+BMI = weight / (height **2)
+print(BMI)
